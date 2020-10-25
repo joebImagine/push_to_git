b21f151c3ed9687dfe143256aabadd103b89c77b
Joe Bates <joebates@jbimagine.com>
2020-10-25 15:42:54 -0700

    clean-up

--- my_utils.py
+++ my_utils.py
@@ -43,7 +43,7 @@ class Utils():
         if os.path.exists(path_name):
             return open(path_name, "r")
         else:
-            self.create_file()
+            self.create_file(path_name)
 
     def create_dir(self, folder_path):
         if os.path.exists(folder_path):
